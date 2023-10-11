4f36e4187b384145298a54e504b0687ccf499255
yun <reve_1997@hotmail.com>
2023-10-11 17:40:29 -0600

    change 10/11/24 v2

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,18 @@
+import { JwtService } from '@nestjs/jwt';
 import { Body, Controller, Post } from '@nestjs/common';
 import { Public } from 'src/decorator/public.decorator';
 import { LoginDto } from 'src/users/dto/login-user.dto';
 import { AuthService } from './auth.service';
+import { PayloadToken } from './payload.interface';
+import { UsersService } from '../users/users.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private jwtService: JwtService,
+    private userService: UsersService,
+  ) {}
 
   @Public()
   @Post('login')
